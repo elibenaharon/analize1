f39b2b1df6ef6bf6dfc1b7d158a190ebe3b67d50
Ilanshk <93129754+Ilanshk@users.noreply.github.com>
2022-03-09 19:11:58 +0200

    changeIlan

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import math
 r = abs(3.0*(4.0/3.0-1)-1)
 print(r)
-e = 1
-while (1+e)!=1:
-    e1 = e
-    e /= 2
+epsilon_machine = 1
+while (1+epsilon_machine)!=1:
+    e1 = epsilon_machine
+    epsilon_machine /= 2
 print(e1)
 print(r-e1)
